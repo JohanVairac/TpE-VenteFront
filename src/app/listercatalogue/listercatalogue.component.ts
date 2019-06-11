d0bd110574ecebc89c8c5954746bc58a3060191d
Elsa GOUZHY <gouzhy_elsa@yahoo.fr>
2019-06-11 14:41:54 +0200

    catalogue

--- src/app/listercatalogue/listercatalogue.component.ts
+++ src/app/listercatalogue/listercatalogue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogueService } from '../catalogue.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listercatalogue',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListercatalogueComponent implements OnInit {
 
-  constructor() { }
+  pageCatalogues: any; // any type angular qui veut dire on peut mettre n'importe quoi
+    constructor(private service: CatalogueService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() { // ngoninit signifie on demarre de la
+        // angular fait du remplissage asynchrone cad
+        this.service.getCatalogues()
+            .subscribe(data => {
+                this.pageCatalogues = data;
+            }, err => {
+                console.log(err);
+            });
+    }
+    deleteProduit(id: number) {
+        this.service.deleteProduit(id)
+            .subscribe(data => this.ngOnInit());
+    }
 
 }
