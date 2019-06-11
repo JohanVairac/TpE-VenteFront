288d9b335283ee3be75400716fdddd77670eb327
Johan VAIRAC <johan.vairac@outlook.fr>
2019-06-11 17:39:42 +0200

    commit app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListerproduitComponent } from './listerproduit/listerproduit.component';
+import { ListercatalogueComponent } from './listercatalogue/listercatalogue.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{path: 'listerproduit', component: ListerproduitComponent},
+{path: 'listercatalogue', component: ListercatalogueComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/listermoyendepaiement/listermoyendepaiement.component.ts
+++ src/app/listermoyendepaiement/listermoyendepaiement.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ProduitService } from '../produit.service';
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { ProduitService } from '../produit.service';
 
-@Component({
-  selector: 'app-listermoyendepaiement',
-  templateUrl: './listermoyendepaiement.component.html',
-  styleUrls: ['./listermoyendepaiement.component.css']
-})
-export class ListermoyendepaiementComponent implements OnInit {
+// @Component({
+//   selector: 'app-listermoyendepaiement',
+//   templateUrl: './listermoyendepaiement.component.html',
+//   styleUrls: ['./listermoyendepaiement.component.css']
+// })
+// export class ListermoyendepaiementComponent implements OnInit {
 
-   pageMoyensdePaiment: any;
-    constructor(private service: ProduitService, private router: Router) { }
+//    pageMoyensdePaiment: any;
+//     constructor(private service: ProduitService, private router: Router) { }
 
-  ngOnInit() {
-      this.service.getProduit()
-        .subscribe(data => {
-            this.pageMoyensdePaiment = data;
-        }, err => {
-            console.log(err);
+//   ngOnInit() {
+//       this.service.getProduit()
+//         .subscribe(data => {
+//             this.pageMoyensdePaiment = data;
+//         }, err => {
+//             console.log(err);
 
-        });
+//         });
 
-}
+// }
