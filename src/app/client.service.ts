ff51e5b8c23a0124d8931a608fded9c776abd23e
Johan VAIRAC <johan.vairac@outlook.fr>
2019-06-12 16:19:28 +0200

    commit client service

---
+++ src/app/client.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+	constructor(private http: HttpClient) {
+
+	
+   }
+  getClient() {
+      return this.http.get('http://localhost:8080/clients/');
+  }
+
+  saveClient(client) {
+      return this.http.post('http://localhost:8080/produits/', client);
+    }
+}
