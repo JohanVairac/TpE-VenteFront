4f4956060ae6f167ba349b8fdc4444c8d70d3a8c
Soulaima MEZGHANI <soulayma.mezghani@gmail.com>
2019-06-13 19:06:07 +0200

    push

--- src/app/listerclient/listerclient.component.ts
+++ src/app/listerclient/listerclient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../client.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listerclient',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListerclientComponent implements OnInit {
 
-  constructor() { }
+  pageClients: any;
+ 	
+  constructor(private service: ClientService , private router: Router) { }
 
   ngOnInit() {
+	   this.service.getClients()
+        .subscribe(data => {
+            this.pageClients = data;
+        }, err => {
+            console.log(err);
+        });
   }
+
 
 }
